3f860f83ad25322bd7bd06b26b17fde33f47b404
JuniorArias02 <junior.arias02yt@gmail.com>
2026-03-12 10:37:47 -0500

    se realiza correicon

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,17 @@ use App\Models\User;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Storage Bridge: Serves storage files even if the symlink is broken (Common for Hostinger)
+Route::get('/storage/{path}', function ($path) {
+    $path = storage_path('app/public/' . $path);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    $file = file_get_contents($path);
+    $type = mime_content_type($path);
+
+    return response($file)->header('Content-Type', $type);
+})->where('path', '.*');
